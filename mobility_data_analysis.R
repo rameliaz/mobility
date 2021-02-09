fb1312265cffdacb72233712db03afc814680aac
rameliaz <amelia.zein@psikologi.unair.ac.id>
2021-02-09 10:17:11 +0700

    dynamic regression

--- mobility_data_analysis.R
+++ mobility_data_analysis.R
@@ -28,34 +28,101 @@ ggpairs(as.data.frame(ds[,10:12]))
 library(forecast)
 library(lubridate)
 
-ds <- ts(ds, start=decimal_date(as.Date("2020-02-15")), frequency = 347)
+ds <- ts(ds, start=decimal_date(as.Date("2020-02-15")), frequency = 365)
 
 
 ## Performing Time Series Linear Regression
-library(car)
+reg.tm.0 <- tslm(daily_cases ~ 1, data=ds)
 
-reg.tm.0 <- tslm(kasus_harian ~ 1, data=ds)
-
-reg.tm.1 <- tslm(kasus_harian ~
+reg.tm.1 <- tslm(daily_cases ~
                    avrg_mobility, data=ds)
 
 summary(reg.tm.1)
 
-reg.tm.2 <- tslm(kasus_harian ~
-                 avrg_mobility + jumlah_tes, confint=T, data=ds)
+reg.tm.2 <- tslm(daily_cases ~
+                 avrg_mobility + testing, data=ds)
 
 summary(reg.tm.2)
-confint(reg.tm.2, level=0.95)
-checkresiduals(reg.tm.2)
-plot(reg.tm)
-naive(ds, bootstrap=T)
 
-arima <- auto.arima(ds[,10], xreg=ds[,11:12])
-summary(arima)
-cbind("Regression Errors" = residuals(arima, type="regression"),
-      "ARIMA errors" = residuals(arima, type="innovation")) %>%
-  autoplot(facets=TRUE)
-checkresiduals(arima)
+### Model Diagnostics
+checkresiduals(reg.tm.2)
+anova(reg.tm.1,reg.tm.2)
+anova(reg.tm.0,reg.tm.1)
+
+resid <-  as.data.frame(ds)
+resid[, "residuals"] <- as.numeric(residuals(reg.tm.2))
+ggplot(resid, aes(x=avrg_mobility, y=residuals)) +
+  geom_point()
+ggplot(resid, aes(x=testing, y=residuals)) +
+  geom_point()
+
+cbind(Fitted = fitted(reg.tm.2),
+      Residuals=residuals(reg.tm.2)) %>% 
+  as.data.frame() %>% 
+  ggplot(aes(x=Fitted, y=Residuals)) + geom_point()
+
+### NOTES: apparently the model violates all regression assumptions, so that we need to use other strategy here. Since the number of cases (our dependent variable) seems to be exponential and additionally, both predictors (testing and mobility) are lagged, Dynamic Regression with Lagged Predictors will be instead used.
+
+## Creating reproducible example
+
+reprex::reprex(input="mobility_data_analysis.R", venue="gh")
+
+
+## Dynamic Regression with Lagged Predictors [Testing]
+
+test <- cbind(
+  testlag6 = stats::lag(ds[,"testing"],-6),
+  testlag8 = stats::lag(ds[,"testing"],-8),
+  testlag14 = stats::lag(ds[,"testing"],-14),
+  testlag20 = stats::lag(ds[,"testing"],-20)) %>%
+  head(NROW(test))
+
+# Restrict data so models use same fitting period
+fit1 <- auto.arima(ds[120:150,10], xreg=test[120:150,1],
+                   stationary=TRUE)
+fit2 <- auto.arima(ds[120:150,10], xreg=test[120:150,1:2],
+                   stationary=TRUE)
+fit3 <- auto.arima(ds[120:150,10], xreg=test[120:150,1:3],
+                   stationary=TRUE)
+fit4 <- auto.arima(ds[120:150,10], xreg=test[120:150,1:4],
+                   stationary=TRUE)
+
+c(fit1[["aicc"]],fit2[["aicc"]],fit3[["aicc"]],fit4[["aicc"]])
+
+
+(fit.0 <- auto.arima(ds[,10], xreg=test[,1],
+                   stationary=TRUE))
+(1-pnorm(abs(fit.0$coef)/sqrt(diag(fit.0$var.coef))))*2
+checkresiduals(fit.0)
+
+## Dynamic Regression with Lagged Predictors [Mobility and Testing]
+
+lag <- cbind(
+  mobilelag6 = stats::lag(ds[,"avrg_mobility"],-6),
+  mobilelag8 = stats::lag(ds[,"avrg_mobility"],-8),
+  mobilelag14 = stats::lag(ds[,"avrg_mobility"],-14),
+  mobilelag20 = stats::lag(ds[,"avrg_mobility"],-20),
+  testlag = stats::lag(ds[,"testing"],-6)) %>%          ## Binding test lag
+  head(NROW(ds))
+
+# Restrict data so models use same fitting period
+fit1 <- auto.arima(ds[120:150,10], xreg=lag[120:150,1],
+                   stationary=TRUE)
+fit2 <- auto.arima(ds[120:150,10], xreg=lag[120:150,1:2],
+                   stationary=TRUE)
+fit3 <- auto.arima(ds[120:150,10], xreg=lag[120:150,1:3],
+                   stationary=TRUE)
+fit4 <- auto.arima(ds[120:150,10], xreg=lag[120:150,1:4],
+                   stationary=TRUE)
+
+c(fit1[["aicc"]],fit2[["aicc"]],fit3[["aicc"]],fit4[["aicc"]])
+
+(fit.1 <- auto.arima(ds[,10], xreg=lag[,c(2,5)],
+                   stationary=TRUE))
+(1-pnorm(abs(fit.1$coef)/sqrt(diag(fit.1$var.coef))))*2
+checkresiduals(fit.1)
+
+c(fit.0[["aicc"]],fit.1["aicc"])
 
 
 ## Data visualization
@@ -66,7 +133,7 @@ autoplot(ds[,3:8], facet=T) +
 
 ds %>% 
   as.data.frame() %>% 
-  ggplot(aes(x=avrg_mobility, y=kasus_harian)) +
+  ggplot(aes(x=avrg_mobility, y=daily_cases)) +
   ylab("Jumlah Kasus Terkonfirmasi Harian") +
   xlab("% Perubahan Mobilitas dari Baseline") +
   geom_point() +
@@ -74,7 +141,7 @@ ds %>%
 
 ds %>% 
   as.data.frame() %>% 
-  ggplot(aes(x=jumlah_tes, y=kasus_harian)) +
+  ggplot(aes(x=testing, y=daily_cases)) +
   ylab("Jumlah Kasus Terkonfirmasi Harian") +
   xlab("Total Jumlah Tes") +
   geom_point() +
